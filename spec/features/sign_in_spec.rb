a632e8bf1835b2adb02b6938bb53deb502a77541
Simon <smoreno@heliostech.fr>
2018-01-10 18:19:46 +0100

    Hotfixes
    - Fixing tests
    - adding missing required JS

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,8 +6,8 @@ describe 'Sign in' do
   it 'allows to sign in with email and password' do
     visit index_path
     click_on 'Sign in'
-    fill_in 'Email', with: account.email
-    fill_in 'Password', with: account.password
+    fill_in 'account_email', with: account.email
+    fill_in 'account_password', with: account.password
     click_on 'Submit'
     expect(page).to have_content("Signed in as #{account.email}.")
   end
