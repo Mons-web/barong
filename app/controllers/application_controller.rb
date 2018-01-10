7a70e2d760fdb8821f354bce2d2177740bba27d8
Simon <smoreno@heliostech.fr>
2018-01-10 15:33:04 +0100

    Adding simple devise forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "web"
+    end
+  end
 end
